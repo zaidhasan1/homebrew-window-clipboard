77306b74fb9bda3ffe4abb462d4402803ddd456d
Zaid hasan <hasanzaid005@gmail.com>
2026-01-08 12:55:33 +0530

    added Cask file for homebrew

---
+++ Cask/window-clipboard.rb
@@ -0,0 +1,11 @@
+cask "window-clipboard" do
+  version "1.0.0"
+  sha256 "3afe7bccf44601241398d903c115920eb163465d3c831ede356ab4523388d1f2"
+
+  url "https://github.com/zaidhasan1/window-clipboard-release/releases/download/clipboard/Window.Clipboard-1.0.0-arm64.dmg"
+  name "Window Clipboard"
+  desc "A simple clipboard history tool designed for a developer’s daily needs."
+  homepage "https://github.com/zaidhasan1/window-clipboard-release"
+
+  app "Window Clipboard.app"
+end
