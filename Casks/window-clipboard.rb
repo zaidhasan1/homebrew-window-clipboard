4ddbb9c0adf07d9d04e8884084ff71c823ccb309
Zaid hasan <hasanzaid005@gmail.com>
2026-01-08 14:44:47 +0530

    changed shasum

--- Casks/window-clipboard.rb
+++ Casks/window-clipboard.rb
@@ -1,6 +1,6 @@
 cask "window-clipboard" do
   version "1.0.0"
-  sha256 "3afe7bccf44601241398d903c115920eb163465d3c831ede356ab4523388d1f2"
+  sha256 "9e3593c4d83452e10b90d86575274a18c1807fa120d23717c8c645e6a854adc8"
 
   url "https://github.com/zaidhasan1/homebrew-window-clipboard/releases/download/clipboard/Window.Clipboard-1.0.0-arm64.dmg"
   name "Window Clipboard"
