d9ee28da8ad56ef03d2f232f8453fc0308821675
Zaid hasan <hasanzaid005@gmail.com>
2026-01-08 13:13:17 +0530

    final correction

--- Casks/window-clipboard.rb
+++ Casks/window-clipboard.rb
@@ -2,10 +2,10 @@ cask "window-clipboard" do
   version "1.0.0"
   sha256 "3afe7bccf44601241398d903c115920eb163465d3c831ede356ab4523388d1f2"
 
-  url "https://github.com/zaidhasan1/homebrew-window-clipboard/releases/download/clipboard/Window.Clipboard-1.0.0-arm64.dmg"
+  url "https://github.com/zaidhasan1/window-clipboard/releases/download/v#{version}/Window%20Clipboard-#{version}-arm64.dmg"
   name "Window Clipboard"
-  desc "A simple clipboard history tool designed for a developer’s daily needs."
-  homepage "https://github.com/zaidhasan1/homebrew-window-clipboard"
+  desc "Simple clipboard history app for developers"
+  homepage "https://github.com/zaidhasan1/window-clipboard"
 
-  app "WindowClipboard.app"
+  app "Window Clipboard.app"
 end
