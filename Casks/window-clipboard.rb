1915518fdc1badef4b946d4eed9c6cd5a72e2d16
Zaid hasan <hasanzaid005@gmail.com>
2026-01-12 13:19:02 +0530

    changed sha256

--- Casks/window-clipboard.rb
+++ Casks/window-clipboard.rb
@@ -1,8 +1,8 @@
 cask "window-clipboard" do
   version "1.0.0"
-  sha256 "99cf21343cf14a23a6024df1984e8fe39a3e171678b59f60d7cf1a3a812b9aa2"
+  sha256 "63c969337e52ddb4a466aff1459249eb0c0a07faaff1331181b943d93f768ab6"
 
-  url "https://github.com/zaidhasan1/homebrew-window-clipboard/releases/download/clipboard/Window.Clipboard-1.0.0-arm64.dmg"
+  url "https://github.com/zaidhasan1/homebrew-window-clipboard/releases/download/window-clipboard/Window.Clipboard-1.0.0-arm64.dmg"
   name "Window Clipboard"
   desc "Simple clipboard history app for developers"
   homepage "https://github.com/zaidhasan1/homebrew-window-clipboard"
